ff444125bda2520c8d926f18c27bab9bc3afdd53
moonjungkimm <152701559+moonjungkimm@users.noreply.github.com>
2024-03-18 17:49:35 +0900

    [level 1] Title: 약수의 합, Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,8 +1,16 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for(int i=1; i<n+1; i++){
-            answer += (n % i == 0) ? i : 0;
+       
+        for(int i=1; i<=Math.sqrt(n); i++){
+            if(n % i == 0){
+                answer += i;
+                if(i != n / i){
+                    answer += n/i;
+                }
+            }
         }
         return answer;
     }
