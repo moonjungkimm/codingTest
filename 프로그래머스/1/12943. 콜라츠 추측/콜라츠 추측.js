f78b1475e40926cf5df74c826b9d823baa2c65ae
Kim moonjung <152701559+moonjungkimm@users.noreply.github.com>
2024-12-20 14:36:22 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,15 @@
+function solution(num) {
+    var answer = 0;
+   
+    while(num != 1){
+        num = num % 2 == 0 ? num / 2 : num * 3 + 1;
+        answer++;
+        
+        
+        if (answer >= 500 ){
+            return -1;
+        }
+    }
+    
+    return answer;
+}
